964537bd287b692cfd58190b51fc7911da1b1815
David Martin <56343699+djmehs17@users.noreply.github.com>
2023-10-26 10:25:45 -0400

    Uploaded scripts for phone number management

---
+++ AddNewUser_V2.ps1
@@ -0,0 +1,28 @@
+﻿#################   Connect to Teams Powershell #########################
+
+
+#################   Collect UPN and Phone Number  and save as variables #########################
+$Identity = Read-Host -Prompt 'Please enter the email address of the employee'
+$LinePre = "tel:"
+$LineURI = Read-Host -prompt 'Please enter the phone number you want to assign to this employee in E164 format(ie 1XXXYYYZZZZ)'
+
+
+
+#################   Enable/Assign Online Phone System For That User   #########################
+Set-CsUser -Id $Identity -EnterpriseVoiceEnabled $true -HostedVoiceMail $true -LineURI $LinePre+$lineuri
+#Assign voice routing policy to user
+Grant-CsTeamsUpgradePolicy -Identity $Identity -PolicyName UpgradeToTeams
+Grant-CsOnlineVoiceRoutingPolicy -PolicyName 'all-traffic-to-sbc' -Identity $Identity
+
+
+#################   Confirm it worked correctly#########################
+Get-CsOnlineUser -Identity $Identity | select DisplayName, *voice*, LineURI, privateline
+
+
+
+
+<######################### CHANGELOG #########################
+Removed "OnPrem" from -OnPremLineURI
+"Set-CsUser -Identity" changed to "Set-CsUser -Id"
+Added "privateline" to the end of the Get-CsOnlineUser command
+#############################################################>
---
+++ Remove Phone Number from User_V2.ps1
@@ -0,0 +1,18 @@
+﻿#################   Connect to Teams Powershell #########################
+
+#################   Collect UPN and save as variable #########################
+$Identity = Read-Host -Prompt 'Please enter the email address of the employee'
+
+
+#################   Disable/Un-Assign Online Phone System For That User   #########################
+set-csuser -Identity $Identity -LineURI $null
+
+
+#################   Confirm it worked correctly#########################
+Get-CsOnlineUser -Identity $Identity  | select DisplayName, *voice*, LineURI
+
+##### Removed "OnPrem" from -OnPremLineURI #####
+
+
+
+
