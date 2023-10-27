4b1a92828632f75902fc2dc613eed33662992fd6
David Martin <56343699+djmehs17@users.noreply.github.com>
2023-10-27 10:06:03 -0400

    Created Check for Phone Number assignment

---
+++ Check For Assigned Phone Numbers.ps1
@@ -0,0 +1,11 @@
+﻿###########################################
+##### VARIABLE CREATION/COLLECT INPUT #####
+###########################################
+#Prompt for employee email and store it as the $Identity variable
+$Identity = Read-Host -Prompt 'Please enter the email address of the employee'
+
+
+###########################
+##### RUN THE COMMAND #####
+###########################
+Get-CsOnlineUser -Identity $Identity | select DisplayName, *voice*, LineURI, privateline
